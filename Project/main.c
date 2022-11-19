33f83d02f8d0d7d95803aacb4e4a17123276b1c3
Nathan Everette <102610368+everettens@users.noreply.github.com>
2022-11-19 17:53:55 +0000

    Initial setup

---
+++ Project/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "project.h"
+
+
+/*
+    Instructions...
+
+
+
+*/
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
---
+++ Project/project.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "project.h"
+/* #include <string.h>  might need this, not sure yet*/
+
+
+/* 
+    Declare static variables, if needed
+*/
+
+
+/*
+    Functions...
+*/
